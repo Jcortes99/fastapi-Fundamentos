37928f325f1e9a30cb61e6b9c42f1f4bba76330b
Jcortes99 <jcortesro99@gmail.com>
2023-04-02 00:08:09 -0500

    changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI, Body, Query
+from fastapi import FastAPI, Body, Query, Path
 from Domain.models import Person
 
 app = FastAPI()
@@ -14,7 +14,25 @@ def create_person(persona: Person = Body(...)):
 
 @app.get("/peron/detail")
 def show_person(
-    name: Optional[str]= Query(None, min_length=1, max_length=10),
-    age: int = Query(...)
+    name: Optional[str]= Query(
+        None,
+        min_length=1,
+        max_length=10,
+        title="Person name",
+        description="The name of the person"
+        ),
+    age: int = Query(...,
+                    title="Person age",
+                    description="Require person age"
+                    )
 ):
     return{name, age}
+
+@app.get("/person/detail/{person_id}")
+def show_person(
+    person_id: int = Path(...,
+                        gt=0,
+                        title="Person id",
+                        description="Require person id")
+):
+    return {person_id: "It exists!"}
